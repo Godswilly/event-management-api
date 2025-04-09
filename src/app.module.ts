273259edd41f94524b1c1ba4e58f1714b850cb11
Godswilly <kalu.agukalu@gmail.com>
2025-04-09 15:45:27 +0100

    setup @nestjs/config with Joi validation, PostgreSQL connection, and Prisma User model

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DATABASE_URL: Joi.string().uri().required(),
+        PORT: Joi.number().default(3000),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
